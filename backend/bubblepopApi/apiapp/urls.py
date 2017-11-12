2233770b7ee2a7a933e44e379d93be7491b5bc0f
Dongkwan Kim <todoaskit@gmail.com>
2017-11-13 00:43:33 +0900

    Add apiapp

--- backend/bubblepopApi/apiapp/urls.py
+++ backend/bubblepopApi/apiapp/urls.py
@@ -13,11 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
+from django.conf.urls import url
+from apiapp import views as view
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^api/', include('apiapp.urls')),
+    url(r'^test', view.test),
 ]
--- backend/bubblepopApi/bubblepopApi/settings.py
+++ backend/bubblepopApi/bubblepopApi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'bn&yfvfm8nz%jrhi5u11v85&u3wo6wpi%+7u91a=)x01434=2z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apiapp',
 ]
 
 MIDDLEWARE = [
