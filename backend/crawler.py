5dde7f62e5c1db9deb988ef8b7f757f1e9f84660
Inseung Hwang <his@sparcs.org>
2017-11-13 16:42:36 +0900

    fix test code

--- backend/crawler.py
+++ backend/crawler.py
@@ -1,12 +1,13 @@
 import feedparser
 from newspaper import Article
+import time
+#from konlpy.tag import Mecab
 
 def get_URLs(rss_link):
     parsed = feedparser.parse(rss_link)
     links = []
     for entry in parsed.entries:
         links.append(entry.link)
-
     return links
 
 def get_article(url):
@@ -16,21 +17,28 @@ def get_article(url):
     return article
 
 def test():
-    file = open("test.txt",'w',encoding="utf8")
     rss_list = [
         #"https://www.reddit.com/",
         "http://www.chosun.com/site/data/rss/politics.xml",
         "http://rss.joins.com/joins_politics_list.xml",
 
         ]
+    #mecab = Macab()
     for rss_link in rss_list:
+        print("Start get_URLs and read files from : "+rss_link)
+        start_time = time.time()
         links = get_URLs(rss_link)
         for link in links:
             article = get_article(link)
-            file.write(article.title)
-            file.write("\n")
-    file.flush()
-    file.close()
+            file = open("./test/%s.txt"%(article.title),
+                    'w',encoding="utf8")
+            #file.write(article.title)
+            #nouns = mecab.nouns(article.content)
+
+            #for noun in nouns:
+            #    file.write("%s\n"%noun)
+            file.close()
+        print("Process time : %f"%(time.time()-start_time))
 
 if __name__ == "__main__":
     test()
