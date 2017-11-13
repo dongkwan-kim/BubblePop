ebec8a6baf46e99860c4e1f425cfb9f7d22b9aee
Dongkwan Kim <todoaskit@gmail.com>
2017-11-13 19:32:26 +0900

    Add AdminModels

--- bubblepopApi/apiapp/admin.py
+++ bubblepopApi/apiapp/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from apiapp.models import Media, Cluster, Article, \
+        CachedArticle, Related, UserBlackList, Report
 
-# Register your models here.
+
+@admin.register(Media)
+class MediaAdmin(admin.ModelAdmin):
+    list_display = ('name', 'rss_list', 'icon')
+
+
+@admin.register(Cluster)
+class ClusterAdmin(admin.ModelAdmin):
+    list_display = ('cluster_id',)
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'media', 'article_url')
+    list_filter = ('media',)
+
+
+@admin.register(CachedArticle)
+class CachedArticleAdmin(admin.ModelAdmin):
+    list_display = ('article', 'cluster')
+    list_filter = ('cluster',)
+
+
+@admin.register(Related)
+class RelatedAdmin(admin.ModelAdmin):
+    list_display = ('article_a', 'article_b', 'similarity', 'reports')
+
+
+@admin.register(UserBlackList)
+class UserBlackListAdmin(admin.ModelAdmin):
+    list_display = ('user', 'media')
+    list_filter = ('user',)
+
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ('user', 'article_a', 'article_b', 'content')
+    list_filter = ('user',)
+
