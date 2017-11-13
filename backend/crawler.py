0d0568e27a9a1ef90ebb42b9244b47ee32b95690
Inseung Hwang <his@sparcs.org>
2017-11-13 17:12:30 +0900

    Extract nouns

--- backend/crawler.py
+++ backend/crawler.py
@@ -1,7 +1,9 @@
 import feedparser
 from newspaper import Article
 import time
-#from konlpy.tag import Mecab
+from konlpy.tag import Hannanum
+from konlpy.tag import Mecab
+
 
 def get_URLs(rss_link):
     parsed = feedparser.parse(rss_link)
@@ -23,22 +25,27 @@ def test():
         "http://rss.joins.com/joins_politics_list.xml",
 
         ]
+    hannanum = Hannanum()
     #mecab = Macab()
     for rss_link in rss_list:
         print("Start get_URLs and read files from : "+rss_link)
         start_time = time.time()
         links = get_URLs(rss_link)
         for link in links:
+            parse_time = time.time()
             article = get_article(link)
             file = open("./test/%s.txt"%(article.title),
                     'w',encoding="utf8")
-            #file.write(article.title)
-            #nouns = mecab.nouns(article.content)
+            nouns = hannanum.nouns(article.text)
+            #nouns = mecab.nouns(article.text)
 
-            #for noun in nouns:
-            #    file.write("%s\n"%noun)
+            for noun in nouns:
+                file.write("%s\n"%noun)
             file.close()
-        print("Process time : %f"%(time.time()-start_time))
+            parse_time = time.time()-parse_time
+            print("parse files from %s: %f"%(link,parse_time))
+        start_time = time.time()-start_time
+        print("Process time : %f"%(start_time))
 
 if __name__ == "__main__":
     test()
