192d8f18b33c12bfe86ee288f2275a978a4d783b
Dongkwan Kim <todoaskit@gmail.com>
2017-11-24 00:51:41 +0900

    Improve readability of models.py

--- bubblepopApi/apiapp/models.py
+++ bubblepopApi/apiapp/models.py
@@ -3,40 +3,87 @@ from django.db import models
 
 
 class Media(models.Model):
-    name = models.CharField(max_length=10, verbose_name='이름')
-    rss_list = models.TextField(verbose_name='RSS 리스트')
-    political_view = models.FloatField(verbose_name='성향')
-    icon = models.URLField(verbose_name='아이콘')
+    name = models.CharField(
+        max_length=10,
+        verbose_name='이름'
+    )
+    rss_list = models.TextField(
+        verbose_name='RSS 리스트'
+    )
+    political_view = models.FloatField(
+        verbose_name='성향'
+    )
+    icon = models.URLField(
+        verbose_name='아이콘'
+    )
 
     def __str__(self):
         return self.name
 
 
 class Cluster(models.Model):
-    cluster_id = models.IntegerField(verbose_name='클러스터 식별자')
+    cluster_id = models.IntegerField(
+        verbose_name='클러스터 식별자'
+    )
 
     def __str__(self):
         return str(self.cluster_id)
 
 
 class Article(models.Model):
-    title = models.TextField(verbose_name='제목')
-    content = models.TextField(verbose_name='본문')
-    morphemed_content = models.TextField(verbose_name='형태소 본문')
-    media = models.ForeignKey(Media, verbose_name='신문사', on_delete=models.CASCADE)
-    writer = models.CharField(max_length=10, verbose_name='작성자', null=True)
-    published_at = models.DateField(verbose_name='발행일')
-    article_url = models.URLField(verbose_name='URL 링크', unique=True)
-    category = models.CharField(max_length=10, verbose_name='분류', null=True)
-    cluster = models.ForeignKey(Cluster, verbose_name='클러스터', null=True, on_delete=models.SET_NULL)
+    title = models.TextField(
+        verbose_name='제목'
+    )
+    content = models.TextField(
+        verbose_name='본문'
+    )
+    morphemed_content = models.TextField(
+        verbose_name='형태소 본문'
+    )
+    media = models.ForeignKey(
+        Media,
+        verbose_name='신문사',
+        on_delete=models.CASCADE
+    )
+    writer = models.CharField(
+        max_length=10,
+        verbose_name='작성자',
+        null=True
+    )
+    published_at = models.DateField(
+        verbose_name='발행일'
+    )
+    article_url = models.URLField(
+        verbose_name='URL 링크',
+        unique=True
+    )
+    category = models.CharField(
+        max_length=10,
+        verbose_name='분류',
+        null=True
+    )
+    cluster = models.ForeignKey(
+        Cluster,
+        verbose_name='클러스터',
+        null=True,
+        on_delete=models.SET_NULL
+    )
 
     def __str__(self):
         return str(self.media) + '-' + self.title
 
 
 class UserBlackList(models.Model):
-    user = models.ForeignKey(User, verbose_name='사용자', on_delete=models.CASCADE)
-    media = models.ForeignKey(Media, verbose_name='언론사', on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User,
+        verbose_name='사용자',
+        on_delete=models.CASCADE
+    )
+    media = models.ForeignKey(
+        Media,
+        verbose_name='언론사',
+        on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return str(self.user) + '-' + str(self.media)
@@ -58,15 +105,32 @@ class Report(models.Model):
         on_delete=models.CASCADE,
         verbose_name='기사B'
     )
-    user = models.ForeignKey(User, verbose_name='사용자', null=True, on_delete=models.SET_NULL)
-    content = models.TextField(verbose_name='제보내용', null=True)
+    user = models.ForeignKey(
+        User,
+        verbose_name='사용자',
+        null=True,
+        on_delete=models.SET_NULL
+    )
+    content = models.TextField(
+        verbose_name='제보내용',
+        null=True
+    )
 
     def __str__(self):
         return str(self.user) + '-' + str(self.id)
 
 
 class UserProfile(models.Model):
-    user = models.ForeignKey(User, verbose_name='사용자', on_delete=models.CASCADE)
-    shown_news = models.IntegerField(default=0, verbose_name='뉴스피드 등장한 뉴스')
-    clicked_news = models.IntegerField(default=0, verbose_name='클릭한 뉴스')
-
+    user = models.ForeignKey(
+        User,
+        verbose_name='사용자',
+        on_delete=models.CASCADE
+    )
+    shown_news = models.IntegerField(
+        default=0,
+        verbose_name='뉴스피드 등장한 뉴스'
+    )
+    clicked_news = models.IntegerField(
+        default=0,
+        verbose_name='클릭한 뉴스'
+    )
