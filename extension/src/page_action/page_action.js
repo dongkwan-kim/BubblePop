bb214f4ee19376842a614dfd9fd03081ae15b6a3
Dongkwan Kim <todoaskit@gmail.com>
2017-11-24 23:26:31 +0900

    Add mediaJSON for real deploy - NOW_TEST = true;

--- extension/src/page_action/page_action.js
+++ extension/src/page_action/page_action.js
@@ -1,38 +1,84 @@
 /* Make this boolean false for production */
-NOW_TEST = true;
+NOW_TEST = false;
 
 var testMediaJSON = [
-    {
-        "name": "조선일보",
-        "affinity": -1,
-        "icon": ""
-    },
-    {
-        "name": "한겨레신문",
-        "affinity": 1,
-        "icon": ""
-    },
-    {
-        "name": "조선일보",
-        "affinity": -1,
-        "icon": ""
-    },
-    {
-        "name": "한겨레신문",
-        "affinity": 1,
-        "icon": ""
-    },
 ];
 
 var mediaJSON = [
-];
+    {
+        "name": "조선일보",
+        "affinity": -0.11,
+        "rss":"http://www.chosun.com/site/data/rss/politics.xml",
+        "icon": "../../icons/조선일보.jpg"
+    },
+    {
+        "name": "중앙일보",
+        "affinity": -0.03,
+        "rss": "http://rss.joins.com/joins_politics_list.xml",
+        "icon": "../../icons/중앙일보.jpg"
+    },
+    {
+        "name": "동아일보",
+        "affinity": 0.01,
+        "rss": "http://rss.donga.com/politics.xml",
+        "icon": "../../icons/동아일보.jpg"
+    },
+    {
+        "name": "매일경제",
+        "affinity": 0.14,
+        "rss": "http://file.mk.co.kr/news/rss/rss_30200030.xml",
+        "icon": "../../icons/매일경제.jpg"
+    },
+    {
+        "name": "한국경제",
+        "affinity": -0.19,
+        "rss": "http://rss.hankyung.com/new/news_politics.xml",
+        "icon": "../../icons/한국경제.png"
+    },
+    {
+        "name": "한겨레신문",
+        "affinity": 0.53,
+        "rss": "http://www.hani.co.kr/rss/politics/",
+        "icon": "../../icons/한겨레신문.jpg"
+    },
+    {
+        "name": "경향신문",
+        "affinity": 0.32,
+        "rss": "http://www.khan.co.kr/rss/rssdata/politic_news.xml",
+        "icon": "../../icons/경향신문.png"
+    },
+    {
+        "name": "오마이뉴스",
+        "affinity": 0.42,
+        "rss": "http://rss.ohmynews.com/rss/politics.xml",
+        "icon": "../../icons/오마이뉴스.png"
+    },
+    {
+        "name": "한국일보",
+        "affinity": 0.37,
+        "rss": "http://rss.hankooki.com/daily/dh_politics.xml",
+        "icon": "../../icons/한국일보.jpg"
+    },
+    {
+        "name": "세계일보",
+        "affinity": -0.18,
+        "rss": "http://rss.segye.com/segye_politic.xml",
+        "icon": "../../icons/세계일보.jpg"
+    },
+    {
+        "name": "국민일보",
+        "affinity": 0.18,
+        "rss": "http://rss.kmib.co.kr/data/kmibPolRss.xml",
+        "icon": "../../icons/국민일보.jpg"
+    }
+]
 
 document.addEventListener("DOMContentLoaded", function(event) {
     addClickListenerToUpdate();
     if (NOW_TEST) {
         addMediaCollection(testMediaJSON);
     } else {
-        // TODO
+        addMediaCollection(mediaJSON);
     }
 });
 
@@ -87,6 +133,7 @@ function addCheckedToMediaJSON(mediaJSON) {
         var blacklist = ['조선일보'];
     } else {
         // TODO fetch blacklist from server
+        var blacklist = [];
     }
     return mediaJSON.map((x) => {
         x.checked = (blacklist.indexOf(x.name) == -1);
