b1ab6a8da427cc00be79605b98799806e1bb1f99
Dongkwan Kim <todoaskit@gmail.com>
2017-11-27 19:31:51 +0000

    Reimplement auth API w/ token

--- bubblepopApi/session/views.py
+++ bubblepopApi/session/views.py
@@ -9,35 +9,53 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def user_login(request):
+
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(username=username,password=password)
-        if user is not None:
-            print('auth')
-            login(request,user)
+        user = authenticate(username=username, password=password)
 
+        if user is not None:
+            login(request,user)
+            profile = UserProfile.objects.get(user=user)
             black_list = UserBlackList.objects.filter(user=user)
+
             return JsonResponse({
                 'result':True,
-                'black_list':[n.media.mid for n in list(black_list)]})
-    return JsonResponse({'result':False,'black_list':[]})
+                'black_list':[n.media.mid for n in list(black_list)],
+                'token': profile.save_token(),
+            })
+
+    return JsonResponse({
+        'result': False,
+        'black_list': [],
+    })
+
 
 @csrf_exempt
 def user_register(request):
-    #print(request)
-    #print(request.POST)
+
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         password_check = request.POST['password_check']
+
         if User.objects.filter(username=username).exists():
-            return JsonResponse({'result':False,'duplicated':True})
-        user = User.objects.create_user(username=username,password=password)
-        UserProfile.objects.create(user=user)
-        return JsonResponse({'result':True,'duplicated':False})
+            return JsonResponse({
+                'result': False,
+                'duplicated': True
+            })
 
+        user = User.objects.create_user(
+                username=username,
+                password=password
+        )
+        profile = UserProfile.objects.create(user=user)
+        token = profile.save_token()
 
-
-
+        return JsonResponse({
+            'result': True,
+            'duplicated': False,
+            'token': token,
+        })
 
