0fabfad233cb2abe3e42982969e59b8ebf37856c
Dongkwan Kim <todoaskit@gmail.com>
2017-11-27 20:17:59 +0900

    Finalize main interface features - Report interface - Remove thumbnail - icon, media name

--- extension/src/inject/inject.js
+++ extension/src/inject/inject.js
@@ -204,40 +204,30 @@ function fetchSSDVLink(link) {
     // TODO
 }
 
-function embeddedArticle(url, thumbnail, title, description, media, icon) {
+function embeddedArticle(url, title, description, media, icon) {
     var template =
-    '<div>'+
-        '<div class="valign-wrapper embedded-title">'+
+    '<div class="row article">'+
+        '<div class="embedded-title col s2">'+
             `<img class="circle" src="${icon}">`+
-            `<span class="media-title">${media}</span>`+
+            `<div class="media-title">${media}</div>`+
+            `<div><a data="${url}" class="report-btn">오류 신고</a></div>`+
         '</div>'+
-        '<div class="embedded-article _6m2 _1zpr clearfix _dcs _4_w4 _59ap _5qqr" data-ft="{&quot;tn&quot;:&quot;H&quot;}">'+
+        '<div class="embedded-article col s10 _6m2 _1zpr clearfix _dcs _4_w4 _59ap _5qqr" data-ft="{&quot;tn&quot;:&quot;H&quot;}">'+
             '<div class="clearfix _2r3x">'+
                 '<div class="lfloat _ohe">'+
                     '<span class="_3m6-">'+
-                        '<div class="_6ks">'+
-                            `<a href="${url}" tabindex="-1" target="_blank">`+
-                                '<div class="_6l- __c_">'+
-                                    '<div class="uiScaledImageContainer fbStoryAttachmentImage" style="max-width:474px;max-height:247px;">'+
-                                        `<img class="scaledImageFitWidth img" src="${thumbnail}" style="top:0px;" alt="" width="474" height="248">`+
-                                    '</div>'+
-                                '</div>'+
-                            '</a>'+
-                        '</div>'+
-                        '<div class="_3ekx _29_4"><div class="_6m3 _--6">'+
+                        '<div class="_3ekx _29_4"><div class="_6m3 _--6 my_6m3">'+
                             '<div class="mbs _6m6 _2cnj _5s6c">'+
                                 `<a href="${url}" target="_blank">${title}</a>`+
                             '</div>'+
-                            `<div class="_6m7 _3bt9">${description}</div>`+
-                            `<div class="_59tj _2iau"><div class="_6lz _6mb ellipsis">${media}</div></div>`+
+                            `<div class="_3bt9 my_3bt9">${description}</div>`+
                         '</div>'+
                         `<a class="_52c6 SSDV-no-need" href="${url}" tabindex="-1" target="_blank"></a>`+
                         '</div>'+
                     '</span>'+
                 '</div>'+
-                '<div class="_42ef"><span class="_3c21"></span></div>'+
             '</div>'+
-        '</div>'
+        '</div>'+
     '</div>'
     return template;
 }
@@ -245,7 +235,6 @@ function embeddedArticle(url, thumbnail, title, description, media, icon) {
 function testEmbeddedArticle() {
     return embeddedArticle(
         "http://news.chosun.com/site/data/html_dir/2017/11/11/2017111100920.html?Dep0=facebook&topics",
-        "https://external.ficn2-1.fna.fbcdn.net/safe_image.php?d=AQAsZZZ0CtUXdOgv&w=476&h=249&url=http%3A%2F%2Fimage.chosun.com%2Fsitedata%2Fimage%2F201711%2F11%2F2017111100884_0.jpg&cfs=1&upscale=1&fallback=news_d_placeholder_publisher&_nc_hash=AQDSTO4vPjj7uJ54",
         '20대 10명中 7명 "빼빼로데이 비용 부담 돼"',
         "20대 10명 중 7명은 빼빼로데이를 챙기는 것에 부담을 느끼는 것으로 조사됐다. 구인사이트 알바천국이 10월 27일부터 지난 9일까지 20대 ..",
         "조선일보",
